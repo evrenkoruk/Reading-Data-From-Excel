78a6c94624279d947c73bf724eb76fb27349ed8e
EMRE DOĞANGÜN <50294340+Emredogangun@users.noreply.github.com>
2021-10-01 18:34:12 +0300

    Created

---
+++ ReadingDataFromExcel/ReadingDataFromExcel/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ReadingDataFromExcel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Read read = new Read();
+            read.button1_Click();
+        }
+    }
+}
---
+++ ReadingDataFromExcel/ReadingDataFromExcel/Read.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Data.OleDb;
+
+namespace ReadingDataFromExcel
+{
+    public class Read
+    {
+
+        public  void button1_Click()
+        {
+
+            try
+            {
+                System.Data.OleDb.OleDbConnection Baglanti;
+                System.Data.OleDb.OleDbCommand myCommand = new System.Data.OleDb.OleDbCommand();
+
+                string sql = null;
+                //Baglanti =  new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=‪C:\\Users\Emre\\Desktop\\SebzeMeyve-Kopya.xlsx; Extended Properties='Excel 12.0 xml;HDR=YES;'");
+                Baglanti = new OleDbConnection(@"Provider = Microsoft.ACE.OLEDB.12.0; Data Source = C:\\Users\Emre\\Desktop\\SebzeMeyve-Kopya1.xlsx; Extended Properties ='Excel 12.0 xml;HDR=YES;'");
+                Baglanti.Open();
+                myCommand.Connection = Baglanti;
+                sql = "Select * from [Sayfa1$]";
+                myCommand.CommandText = sql;
+                System.Data.OleDb.OleDbDataReader dr = myCommand.ExecuteReader();
+
+                /*datareader içindeki verileri okuma*/
+                while (dr.Read())
+                {
+                    Console.WriteLine(String.Format("Name :{0} ,Barcode:{1} ve Category ID :{2}", dr[0], dr[1], dr[2].ToString()));
+                }
+                Baglanti.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+        }
+    }
+}
